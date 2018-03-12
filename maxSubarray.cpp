eb12b1c95851bbfc07c510b0edd64f800a56c376
Nhan Cao <caotrongduynhan@gmail.com>
2018-03-12 08:33:45 -0500

    Update maxSubarray.cpp

--- maxSubarray.cpp
+++ maxSubarray.cpp
@@ -3,57 +3,15 @@ using namespace std;
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        vector<int> begin;
-        vector<int> end;
-        int max=0;
-        
-        bool zero=false;
+        if(!nums.size())
+            return 0;
+        int sum=nums[0], maxS=nums[0];
+        for(int i=1; i<nums.size(); i++) {
 
-        for(int i=0; i<nums.size(); i++) {
-            if(nums[i]==0) {
-                nums.erase(nums.begin()+i);
-                i--;
-                zero=true;
-            }
+            sum=nums[i]+max(sum, 0);
+            maxS=max(sum, maxS);
         }
 
-        for(int i=0; i<nums.size(); i++) {
-            if(nums[i]>0 && (i==0 || nums[i-1]<0))
-                begin.push_back(i);
-            if(nums[i]>0 && (i==nums.size()-1 || nums[i+1]<0))
-                end.push_back(i);
-        }
-
-        if(begin.size()==0) {
-            if(zero)
-                return 0;
-            return *max_element(nums.begin(), nums.end());
-        }
-
-        for(int bIndex=0; bIndex<begin.size(); bIndex++) {
-            int eIndex=0;
-
-            while(end[eIndex]<begin[bIndex])
-                eIndex++;
-            int sum=nums[begin[bIndex]];
-
-            for(int each=begin[bIndex]+1; each<=end[eIndex]; each++) {
-                sum+=nums[each];
-            }
-
-            if(sum>max)
-                max=sum;
-
-            for(eIndex; eIndex<end.size()-1; eIndex++) {
-                for(int each=end[eIndex]+1; each<=end[eIndex+1]; each++) {
-                    sum+=nums[each];
-                }
-                if(sum>max)
-                    max=sum;
-            }
-        }
-        
-        return max;
+        return maxS;
     }
 };
-
